427a72b5ec1dc98a69555249d8102b3145e3bf62
lucifer <saurabhsinha099@gmail.com>
2022-08-03 15:16:11 +0530

    minor change

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -8,7 +8,7 @@ const NewsItem = (props) => {
                 <div className="card-body shadow-lg p-3 bg-body-primary rounded ">
                     <h5 className="card-title">{props.title}...</h5>
                     <p className="card-text">{props.description}...</p>
-                    <p className="card-text"><small className="text-muted">By {props.author ? props.author : "unknown"} on {props.Date}</small></p>
+                    <p className="card-text"><small className="text-muted">Written by {props.author ? props.author : "unknown"} on {props.Date}</small></p>
                     <a href={props.newslink ? props.newslink : "https://www.google.com/"} target="blank" className="btn btn-sm btn-dark">Read More</a>
                 </div>
             </div>
